aa8eb4212fb4458eac795a39cda12936a90405b3
Jeremy Jones <j1-aggie@hotmail.com>
2020-09-16 22:51:05 -0500

    Update config.py
    removed keys

--- Panda Files/config.py
+++ Panda Files/config.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "486a3f0a342100df108bae3ce4d66f0e"
+weather_api_key = ""
                    
 # Google API Key
-g_key = "AIzaSyCYb0jNY3OgTKmwyHDqVvlfZe14ybLRQ3k"
+g_key = ""
